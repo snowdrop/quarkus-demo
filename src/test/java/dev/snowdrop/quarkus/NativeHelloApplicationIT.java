98e28c6b60602d7ca68fab57dda35db0396551e8
Charles Moulliard <cmoulliard@redhat.com>
2020-01-20 10:54:55 +0100

    remove code created with instructions

--- src/test/java/dev/snowdrop/quarkus/NativeHelloApplicationIT.java
+++
@@ -1,9 +0,0 @@
-package dev.snowdrop.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeHelloApplicationIT extends HelloApplicationTest {
-
-    // Execute the same tests but in native mode.
-}
